e0fc8c71383d08464d7340b6953a97f97a28303b
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2023-09-02 04:35:01 +0200

    ole

---
+++ README.md
@@ -0,0 +1,19 @@
+# CodeChest 3ntr0pia
+
+# English:
+
+CodeChest 3ntr0pia is a VS Code extension that allows you to select and add multiple code files to a "chest". Then, you can copy all their content into a single file named YourCode.txt.
+
+# How to Use:
+
+Add to CodeChest: Select the code files you want to add to the chest.
+Copy from CodeChest: Once you've added all the desired files, execute this command to copy all the content into YourCode.txt.
+
+# Español:
+
+CodeChest 3ntr0pia es una extensión de VS Code que te permite seleccionar y agregar múltiples archivos de código a un "cofre". Luego, puedes copiar todo su contenido en un único archivo llamado YourCode.txt.
+
+# Cómo usar:
+
+Agregar a CofreDelCodigo: Selecciona los archivos de código que deseas agregar al cofre.
+Copiar desde CofreDelCodigo: Una vez que hayas agregado todos los archivos deseados, ejecuta este comando para copiar todo el contenido en YourCode.txt.
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+let codeChest: string[] = [];
+
+export function activate(context: vscode.ExtensionContext) {
+    let addToCodeChest = vscode.commands.registerCommand('extension.addToCodeChest', async () => {
+        // Muestra un cuadro de diálogo para que el usuario seleccione archivos
+        const files = await vscode.window.showOpenDialog({
+            canSelectMany: true,
+            openLabel: 'Add to CodeChest || Añadir al CodeCofre'
+        });
+
+        if (!files || files.length === 0) {
+            vscode.window.showWarningMessage('No files selected.  || No hay archivos seleccionados');
+            return;
+        }
+
+        for (const file of files) {
+            codeChest.push(file.fsPath);
+            vscode.window.showInformationMessage(`Added ${path.basename(file.fsPath)} to CodeChest.`);
+        }
+    });
+
+    let copyFromCodeChest = vscode.commands.registerCommand('extension.copyFromCodeChest', () => {
+        if (codeChest.length === 0) {
+            vscode.window.showWarningMessage('CodeChest is empty. || El CodeCofre esta vacio!');
+            return;
+        }
+
+        let content = '';
+        for (const filePath of codeChest) {
+            const fileName = path.basename(filePath);
+            const fileContent = fs.readFileSync(filePath, 'utf8');
+            content += `// ${fileName}\n${fileContent}\n\n`;
+        }
+
+        const workspacePath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : null;
+        const savePath = path.join(workspacePath || '', 'YourCodeChest.txt');
+        fs.writeFileSync(savePath, content);
+        vscode.window.showInformationMessage(`Files content copied from CodeChest to ${savePath} || Todo copiado al CodeCofre`);
+
+        // Clear the CodeChest
+        codeChest = [];
+    });
+
+    context.subscriptions.push(addToCodeChest, copyFromCodeChest);
+}
